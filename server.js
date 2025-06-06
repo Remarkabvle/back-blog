1ce10c2b7d49364eab782d575412933403a9dc77
abubakr <abubakrdev69@gmail.com>
2025-06-06 08:59:02 +0500

    fix

--- server.js
+++ server.js
@@ -2,12 +2,19 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const connectDB = require("./config/db");
+const fs = require("fs");
+const path = require("path");
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 connectDB();
+
+const uploadDir = path.join(__dirname, "uploads");
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir);
+}
 
 app.use(cors());
 app.use(express.json());
